2720b465ff10966255dd0be9eeeb5e873cca8db1
金健翔 <andy_ios@163.com>
2015-01-14 22:33:51 +0800

    添加.gitignore

---
+++ LBSDemo/LBSDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LBSDemo
+//
+//  Created by andy on 1/14/15.
+//  Copyright (c) 2015 andy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # map-practice
-高德地图SDK
+
+高德地图SDK练习
+
+## 准备工作
+
+1. [注册开发者账号](http://lbs.amap.com/console/)
+*  [官网下载SDK](http://lbs.amap.com/api/ios-sdk/down)
+*  创建iOS项目、集成SDK
